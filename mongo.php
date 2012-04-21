1fbcaacbb84b93cbc473a5b71ce807241aa6879d
Bilal Quadri <bilalquadri92@gmail.com>
2012-04-21 17:28:23 -0400

    Set up database for localhost

--- mongo.php
+++ mongo.php
@@ -1,8 +1,9 @@
 <?php
 try {
-  $m = new Mongo(); // connect
+  //$m = new Mongo("mongodb://<username>:<password>@staff.mongohq.com:10038/app4191123"); // connect
   //$db = $m->learn;
   //$collection = $db->names;
+  $m = new Mongo();
   $db = $m->selectDB('app4191123');
   $collection = $db->selectCollection('names');  
 }
